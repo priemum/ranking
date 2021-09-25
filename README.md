464abb01bd7651f960b9d073653ba00b96020bc8
L-architec-T <development.larchitect@gmail.com>
2021-09-25 23:31:05 +0200

    Le premier commit

---
+++ README.md
@@ -0,0 +1,55 @@
+<h2 align="center">
+  DiscordInvites Ranking<br/>
+  <a href="#" style="font-size: 5px;">By LarchitecT</a>
+</h2>
+
+<br/>
+
+
+<a href="https://github.com/L-architec-T/ranking/stargazers" target="_blank">
+  <img align="left" alt="Discord" src="https://img.shields.io/github/stars/L-architec-T/ranking?color=red&logo=github&style=for-the-badge" width=15%">
+</a>
+
+<a href="https://github.com/L-architec-T/ranking/network" target="_blank">
+  <img align="right" alt="Discord" src="https://img.shields.io/github/forks/L-architec-T/ranking?color=red&logo=github&style=for-the-badge" width=15%">
+</a>
+
+
+<br></br>
+
+<h3 align="center">
+    🔹
+    <a href="https://github.com/L-architec-T/ranking/issues">Report Bug</a> &nbsp; &nbsp;
+    🔹
+    <a href="https://github.com/L-architec-T/ranking/issues">Request Feature</a>
+</h3>
+
+## 🛠 Instructions
+
+1. Renseignez les informations nécessaires dans le fichier `.env`
+2. `npm i`
+3. `npm i pm2 -g`
+4. `pm2 start index.js -n "RANKING"`
+
+## ❓ Que fait-il ?
+
+1. Il surveillera le salon des votes **DiscordInvites** correspondant à l'ID que vous renseignerez dans la zone `channelGet` dans le fichier `.env`.
+2. Tous les votes en cours seront inscrit dans le fichier `db.sqlite`.
+3. Tous les **30 du mois**, le Bot affichera les votes récoltés au cours du mois dans le salon correspondant à l'ID que vous renseignerez dans la zone `channelSpan` dans le fichier `.env`.
+
+Pour une meilleure visibilité des membres ayant voté plus d'un certain nombre de fois, indiqué le nombre de votes souhaité dans la zone `votesNumber` dans le fichier `.env`.
+
+##
+
+<p align="center" width="100%">
+    <img width="50%" src="https://cdn.discordapp.com/attachments/863195363346743327/891435381097107516/Capture_decran_de_2021-09-25_23-25-05.png">
+</p>
+
+##
+
+<h2 align="center">
+  Montre ton soutien
+
+<br/>
+  <a href="https://github.com/L-architec-T/ranking/stargazers" style="font-size: 5px;">Donne une ⭐ si ta kiffé !</a>
+</h2>
---
+++ index.js
@@ -0,0 +1,236 @@
+const Discord = require('discord.js')
+require('dotenv').config()
+
+const client = new Discord.Client();
+const color = require('chalk');
+const fs = require('fs');
+const cron = require('cron');
+const moment = require('moment');
+const sql = require("sqlite");
+sql.open('db.sqlite', { Promise, min: 2, max: 10 }).then(sql => {
+    sql.run('PRAGMA busy_timeout = 2000');
+    sql.configure("busyTimeout", 2000);
+});
+
+let options = { month: 'long'};
+let date = new Date();
+let i = 1;
+
+client.on('ready', async () => {
+	console.log(`LE BOT EST ${color.green("EN LIGNE")} !!\n======================`);
+
+	let channel = client.channels.cache.get(process.env.channelSpan);
+    let scheduledMessage = new cron.CronJob(`0 0 30 * *`, async() => { // 00 */1 * * * * Toute les minutes
+    let profile = await sql.get('SELECT * FROM users');
+    let profileAll = await sql.all('SELECT * FROM users ORDER BY votes DESC');
+    let result = []
+
+        await profileAll.forEach(async data => {
+            let checker;
+            if (data.votes > process.env.votesNumber) {
+                checker = "✅"
+            } else {
+                checker = "❌"
+            }
+            await result.push(`**${data.id}** • ${checker} ` + '`' + data.votes + '`' + ' votes ' + 'de ' + '<@' + data.userId + '> ' + `(\`${data.userName}\`)`)
+        })
+
+        if(result.length < 1) {
+            await channel.send({
+                embed: {
+                    color: 0x2F3136,
+                    title: `Classement des votes du mois de ${new Intl.DateTimeFormat('fr-FR', options).format(date)}`,
+                    description: '`Aucun participant !!`',
+                    image: {
+                        url: 'https://cdn.discordapp.com/attachments/590892043245584396/861393043995623444/ezgif.com-gif-maker.gif'
+                    },
+                    footer: {
+                        text: `${process.env.votesNumber} votes à atteindre`
+                    }
+                }
+            })
+        } else {
+            await channel.send({
+                embed: {
+                    color: 0x2F3136,
+                    title: `Classement des votes du mois de ${new Intl.DateTimeFormat('fr-FR', options).format(date)}`,
+                    description: result.join('\n'),
+                    image: {
+                        url: 'https://cdn.discordapp.com/attachments/590892043245584396/861393043995623444/ezgif.com-gif-maker.gif'
+                    },
+                    footer: {
+                        text: `${process.env.votesNumber} votes à atteindre`
+                    }
+                }
+            })
+        }
+
+        await sql.run('DROP TABLE users').then(() => {
+            sql.run('CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, userId TEXT, userName TEXT, votes INTEGER)')
+            console.log("REINITIALISED Database !!")
+        });
+
+    });
+
+    scheduledMessage.start();
+
+})
+
+client.on('message', async message => {
+
+    if (message.content.startsWith(process.env.prefix + "rank")) {
+        let profileAll = await sql.all('SELECT * FROM users ORDER BY votes DESC')
+        let result = []
+
+        await profileAll.forEach(async data => {
+
+            let checker;
+            if (data.votes > process.env.votesNumber) {
+                checker = "✅"
+            } else {
+                checker = "❌"
+            }
+
+            await result.push(`**${data.id}** • ${checker} ` + '`' + data.votes + '`' + ' votes ' + 'de ' + '<@' + data.userId + '> ' + `(\`${data.userName}\`)`);
+
+        })
+
+        let totalPage = Math.ceil(result.length / 10);
+        if (totalPage < 1) totalPage = 1;
+
+        if(result.length < 1) {
+            message.channel.send({
+                embed: {
+                    color: 0x2F3136,
+                    title: `Classement des votes du mois de ${new Intl.DateTimeFormat('fr-FR', options).format(date)}`,
+                    description: '`Aucun participant`',
+                    image: {
+                        url: 'https://cdn.discordapp.com/attachments/590892043245584396/861393043995623444/ezgif.com-gif-maker.gif'
+                    }
+                }
+            })
+        } else {
+
+            (await async function () {
+                let x = 1;
+                let p0 = 0;
+                let p1 = 10;
+
+                let noOfPages = result.length / 10;
+                let p = (0 > 0 && 0 < noOfPages + 1) ? 0 : 1;
+                let c = (0 > 0 && 0 < noOfPages + 1) ? 0 : 0;
+
+                const data = await message.channel.send({
+                    embed: {
+                        color: 0x2F3136,
+                        title: `Classement des votes du mois de ${new Intl.DateTimeFormat('fr-FR', options).format(date)}`,
+                        description: result.slice(0, 10).join('\n'),
+                        image: {
+                            url: 'https://cdn.discordapp.com/attachments/590892043245584396/861393043995623444/ezgif.com-gif-maker.gif'
+                        },
+                        footer: {
+                            text: `${x} sur ${totalPage} pages • ${process.env.votesNumber} votes à atteindre`
+                        }
+                    }
+                })
+
+                await data.react("⬅");
+                await data.react("➡");
+                await data.react("❌");
+
+                const data_res = data.createReactionCollector((reaction, user) => user.id === message.author.id);
+
+                data_res.on("collect", async (reaction, user) => {
+
+                    if (reaction.emoji.name === "⬅") {
+                        if (x != 0) x--;
+
+                        p0 = p0 - 10;
+                        p1 = p1 - 10;
+
+                        if (p0 < 0) {
+                            return data.delete();
+                        }
+                        if (p0 === undefined || p1 === undefined) {
+                            return data.delete();
+                        }
+
+                        data.edit({
+                            embed: {
+                                color: 0x2F3136,
+                                title: `Classement des votes du mois de ${new Intl.DateTimeFormat('fr-FR', options).format(date)}`,
+                                description: result.slice(p0, p1).join('\n'),
+                                image: {
+                                    url: 'https://cdn.discordapp.com/attachments/590892043245584396/861393043995623444/ezgif.com-gif-maker.gif'
+                                },
+                                footer: {
+                                    text: `${x} sur ${totalPage} pages • ${process.env.votesNumber} votes à atteindre`
+                                }
+                            }
+                        })
+                    }
+
+                    if (reaction.emoji.name === "➡") {
+                        x++;
+
+                        p0 = p0 + 10;
+                        p1 = p1 + 10;
+
+                        if (p1 > result + 10) {
+                            return data.delete();
+                        }
+                        if (p0 === undefined || p1 === undefined) {
+                            return data.delete();
+                        }
+
+                        data.edit({
+                            embed: {
+                                color: 0x2F3136,
+                                title: `Classement des votes du mois de ${new Intl.DateTimeFormat('fr-FR', options).format(date)}`,
+                                description: result.slice(p0, p1).join('\n'),
+                                image: {
+                                    url: 'https://cdn.discordapp.com/attachments/590892043245584396/861393043995623444/ezgif.com-gif-maker.gif'
+                                },
+                                footer: {
+                                    text: `${x} sur ${totalPage} pages • ${process.env.votesNumber} votes à atteindre`
+                                }
+                            }
+                        })
+                    }
+
+                    if (reaction.emoji.name === "❌") {
+                        return data.delete(data);
+                    }
+
+                    await reaction.users.remove(user)
+
+                })
+            }())
+        }
+    }
+
+    try {
+
+        if (message.channel.id !== process.env.channelGet) return;
+
+        if (message.embeds.length > 0) {
+            let embedContent = message.embeds[0].description;
+            let memberId = embedContent.slice(2, 20);
+            let memberUsername = embedContent.split('**')[1];
+            let numberVotes = embedContent.split('`')[1];
+            let profile = await sql.get('SELECT userId FROM users WHERE userId="' + memberId + '"');
+
+            if (!profile) {
+                await sql.run('INSERT INTO users (userId, userName, votes) VALUES (?, ?, ?)', [memberId, memberUsername, numberVotes]);
+                await console.log(memberId + " INSERT " + numberVotes + " Votes ");
+            } else {
+                await sql.run('UPDATE users SET votes="' + numberVotes + '"WHERE userId = "' + memberId + '"');
+                await console.log(memberId + " UPDATE " + numberVotes + " Votes");
+            }
+        }
+    } catch(e) {
+        console.log(e)
+    }
+})
+
+client.login(process.env.token).catch(() => console.log("TOKEN INVALIDE !!"));
